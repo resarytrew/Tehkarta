c37ac487ba463b241a6b9c9ea66101a4d2dadd0f
resarytrew <resarytrew@gmail.com>
2026-08-24 10:55:36 +0500

    feat(infra): expose worker timer outputs

--- infra/terraform/modules/serverless-worker/outputs.tf
+++ infra/terraform/modules/serverless-worker/outputs.tf
@@ -12,3 +12,13 @@ output "runtime_service_account_id" {
   value       = yandex_iam_service_account.runtime.id
   description = "Worker runtime service account ID."
 }
+
+output "trigger_id" {
+  value       = var.enable_timer ? yandex_function_trigger.worker_timer[0].id : null
+  description = "Timer trigger ID when scheduled invocation is enabled."
+}
+
+output "trigger_service_account_id" {
+  value       = var.enable_timer ? yandex_iam_service_account.trigger[0].id : null
+  description = "Least-privilege timer invocation service account ID."
+}
