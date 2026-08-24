410897c0d765f943847ff5550397cf926b41b172
resarytrew <resarytrew@gmail.com>
2026-08-24 10:36:07 +0500

    feat(infra): expose worker registry outputs

--- infra/terraform/modules/registry/outputs.tf
+++ infra/terraform/modules/registry/outputs.tf
@@ -12,3 +12,13 @@ output "api_repository_path" {
   value       = "cr.yandex/${yandex_container_registry.this.id}/${var.api_repository_name}"
   description = "Registry path used when tagging and deploying the API image."
 }
+
+output "worker_repository_id" {
+  value       = yandex_container_repository.worker.id
+  description = "Worker repository ID."
+}
+
+output "worker_repository_path" {
+  value       = "cr.yandex/${yandex_container_registry.this.id}/${var.worker_repository_name}"
+  description = "Registry path used when tagging and deploying the worker image."
+}
